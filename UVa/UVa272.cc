fb9ab55a5c5339b711c0a7b4ec62ab565a87718a
zlsh80826 <zlsh80826@gmail.com>
2017-04-02 16:10:18 +0800

    UVa 272

---
+++ UVa/UVa272.cc
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(int argc, char const *argv[]) {
+    char in;
+    bool flag = true;
+
+    while ( (in = getchar()) != EOF ) {
+        if (in == '"') {
+            printf("%s", flag ? "``" : "''");
+            flag = !flag;
+        } else {
+            printf("%c", in);
+        }
+    }
+
+    return 0;
+}
