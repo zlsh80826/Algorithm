ebcd7dd3007bacb1d43dd7c06c57f61ef2447190
zlsh80826 <zlsh80826@gmail.com>
2017-04-03 14:07:48 +0800

    Clang-Format

--- C/insertion_sort.cc
+++ C/insertion_sort.cc
@@ -6,34 +6,34 @@ void insertion_sort();
 void print();
 
 int main() {
-	int n, in;
-	std::cin >> n;
+    int n, in;
+    std::cin >> n;
 
-	while( n-- ) {
-		std::cin >> in;
-		arr.push_back(in);
-	}
+    while (n--) {
+        std::cin >> in;
+        arr.push_back(in);
+    }
 
-	insertion_sort();
+    insertion_sort();
 
-	print();
+    print();
 }
 
 void insertion_sort() {
-	for (int i = 1; i < arr.size(); ++ i) {
-		int key = arr[i];
-		int j = i - 1;
-		while(j >= 0 && arr[j] > key) {
-			arr[j + 1] = arr[j];
-			-- j;
-		}
-		arr[j + 1] = key;	
-	}
+    for (int i = 1; i < arr.size(); ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 void print() {
-	for (auto i = arr.begin(); i != arr.end(); ++ i) {
-		std::cout << *i << ' ';
-	}
-	std::cout << '\n';
+    for (auto i = arr.begin(); i != arr.end(); ++i) {
+        std::cout << *i << ' ';
+    }
+    std::cout << '\n';
 }
--- UVa/UVa1584.cc
+++ UVa/UVa1584.cc
@@ -1,19 +1,18 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <queue>
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
     int T;
     std::string str;
     std::cin >> T;
-    while (T --) {
+    while (T--) {
         std::cin >> str;
-        std::priority_queue<std::string, std::vector<std::string>, 
-                            std::greater<std::string> > PQ;
-        for (int i = 0; i < str.size(); ++ i) {
+        std::priority_queue<std::string, std::vector<std::string>, std::greater<std::string>> PQ;
+        for (int i = 0; i < str.size(); ++i) {
             PQ.push(str);
             str.push_back(str[0]);
-            str.erase(str.begin());            
+            str.erase(str.begin());
         }
         std::cout << PQ.top() << std::endl;
     }
